836616e84123f2c1bead20b2735e07a386d00e5c
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-04-06 14:03:57 -0600

    Directives & property/attribute binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'APR06_Directives';
+  title = 'Welcome to Angular Directives!';
+  description = 'This is my description.';
+  isDirect = true;
+  products = ['laptop', 'mobile', 'camera', 'watch'];
+  angularLogo =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png';
+  colSpan = 2;
 }
