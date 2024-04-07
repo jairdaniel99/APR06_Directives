8fed6e0721cb61c86099ed94f9bd2749381cd40b
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-04-07 13:36:37 -0600

    Directives, class and style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,5 @@ export class AppComponent {
   angularLogo =
     'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png';
   colSpan = 2;
+  isActive = false;
 }
